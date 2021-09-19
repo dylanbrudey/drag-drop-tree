051f6212cfd49e0be2101e992888440548ad4507
dylanbrudey <47382098+dylanbrudey@users.noreply.github.com>
2021-09-19 23:53:31 +0200

    Revert "Feat add CORS headers to responses"

--- backend/data/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-from data.models import Store
-from data.serializers import StoreSerializer
-
-
-# ViewSets define the view behavior.
-class StoreViewSet(viewsets.ModelViewSet):
-    queryset = Store.objects.all()
-    serializer_class = StoreSerializer
